b4259e12f6b7b316ed406b6ab3afffe4977f561c
Bernat <bernat@brsrv.world>
2026-06-01 13:37:42 +0200

    fix

--- mcp/start_mcp_bridge.sh
+++ mcp/start_mcp_bridge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pkill ollama
-CUDA_VISIBLE_DEVICES="" ollama serve&
+CUDA_VISIBLE_DEVICES="" ollama serve& #usar cpu, si tienes más memoria vram quita el CUDA_VISIBLE_DEVICES
 python3 mcp_ollama.py
